fd8dfab7b939f45bebd58293d6639fd66ca6a79d
Saasmull <79657472+Saasmull@users.noreply.github.com>
2023-09-26 19:38:10 +0200

    MLG animation

--- assets/scripts/mlg.js
+++ assets/scripts/mlg.js
@@ -78,7 +78,7 @@ class MlgMeme{
         this.element.style.height = "200px";
         this.element.style.backgroundColor = "transparent !important";
         this.element.style.background = "transparent";
-        this.element.style.animation = "none";
+        this.element.style.animation = "shake 200ms linear infinite";
         this.element.style.filter = "none";
         this.element.style.backdropFilter = "none";
         this.src = data.image;
@@ -144,7 +144,8 @@ window.addEventListener("keyup",function(event){
     if(event.key === keyArray[keyIndex]){
         keyIndex++;
         if(keyIndex === keyArray.length){
-            document.head.innerHTML += "<style id=\"mlg\">@keyframes rainbow-bg{0%,100%{background-color:red}8%{background-color:#ff7f00}16%{background-color:#ff0}25%{background-color:#7fff00}33%{background-color:#0f0}41%{background-color:#00ff7f}50%{background-color:#0ff}58%{background-color:#007fff}66%{background-color:#00f}75%{background-color:#7f00ff}83%{background-color:#f0f}91%{background-color:#ff007f}}@keyframes rainbow{0%,100%{color:red!important}8%{color:#ff7f00!important}16%{color:#ff0!important}25%{color:#7fff00!important}33%{color:#0f0!important}41%{color:#00ff7f!important}50%{color:#0ff!important}58%{color:#007fff!important}66%{color:#00f!important}75%{color:#7f00ff!important}83%{color:#f0f!important}91%{color:#ff007f!important}}*{animation:.2s linear infinite rainbow-bg,1.5s linear infinite rainbow}</style>";
+            document.head.innerHTML += "<style id=\"mlg\">@keyframes shake {0% { transform: translate(1px, 1px); }10% { transform: translate(-1px, -2px); }20% { transform: translate(-3px, 0px); }30% { transform: translate(3px, 2px); }40% { transform: translate(1px, -1px); }50% { transform: translate(-1px, 2px); }60% { transform: translate(-3px, 1px); }70% { transform: translate(3px, 1px); }80% { transform: translate(-1px, -1px); }90% { transform: translate(1px, 2px); }100% { transform: translate(1px, -2px); }}"+
+            "@keyframes rainbow-bg{0%,100%{background-color:red}8%{background-color:#ff7f00}16%{background-color:#ff0}25%{background-color:#7fff00}33%{background-color:#0f0}41%{background-color:#00ff7f}50%{background-color:#0ff}58%{background-color:#007fff}66%{background-color:#00f}75%{background-color:#7f00ff}83%{background-color:#f0f}91%{background-color:#ff007f}}@keyframes rainbow{0%,100%{color:red!important}8%{color:#ff7f00!important}16%{color:#ff0!important}25%{color:#7fff00!important}33%{color:#0f0!important}41%{color:#00ff7f!important}50%{color:#0ff!important}58%{color:#007fff!important}66%{color:#00f!important}75%{color:#7f00ff!important}83%{color:#f0f!important}91%{color:#ff007f!important}}*{animation:.2s linear infinite rainbow-bg,1.5s linear infinite rainbow}</style>";
             for(var i = 0;i < memes.length;i++){
                 setTimeout(memes[i].play.bind(memes[i]),Math.floor(Math.random() * 2000));
                 memes[i].onReady(function(event,element){
