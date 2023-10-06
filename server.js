862085a154adb4dcf57755c28e89f24f6ca420cd
Saasmull <79657472+Saasmull@users.noreply.github.com>
2023-10-06 13:59:18 +0200

    Password sec

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const CONFIG = require("./utils/config");
 const express = require("express");
 const app = express();
-const sec = require("express-secretsite");
+const sec = require("express-basic-auth");
 
 app.use(sec({
-    password:"test"
+    users:{
+        "user_journalismusag":"vD6)1w3Bt*w<"
+    },
+    challenge:true
 }))
 app.use("/",express.static(__dirname+"/root"));
 app.listen(CONFIG.PORT);
