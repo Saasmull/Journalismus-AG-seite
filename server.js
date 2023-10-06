2c4d2066cd60408de47f4cb375395c1b2938b812
Saasmull <79657472+Saasmull@users.noreply.github.com>
2023-10-06 13:50:34 +0200

    Password

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const CONFIG = require("./utils/config");
 const express = require("express");
 const app = express();
+const sec = require("express-secretsite");
 
+app.use(sec({
+    password:"test"
+}))
 app.use("/",express.static(__dirname+"/root"));
 app.listen(CONFIG.PORT);
