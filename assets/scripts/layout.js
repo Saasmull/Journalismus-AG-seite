efe6abd0303bdb9be141a2d2dc72fb328d417275
Saasmull <79657472+Saasmull@users.noreply.github.com>
2023-11-22 09:58:53 +0100

    Only remove inactive service workers

--- assets/scripts/layout.js
+++ assets/scripts/layout.js
@@ -130,7 +130,7 @@ if ("serviceWorker" in navigator) {
     });
     navigator.serviceWorker.getRegistrations().then(function(registrations){
         for(var i = 0;i < registrations.length;i++) {
-            if (registrations[i] !== navigator.serviceWorker.controller) {
+            if(!registrations[i].active && registrations[i] !== navigator.serviceWorker.controller) {
                 registrations[i].unregister();
             }
         }
