91da2c3fe59ecef57bb5b518fe529c1d5f65ab20
Saasmull <79657472+Saasmull@users.noreply.github.com>
2023-11-27 12:21:46 +0100

    Only IRI's

--- utils/RssFeed.js
+++ utils/RssFeed.js
@@ -23,9 +23,9 @@ module.exports = class RssFeed{
      */
     renderItem(article){
         var xml = CONFIG.INDENT + "<item>" + CONFIG.BREAK;
-        xml += CONFIG.INDENT + CONFIG.INDENT + "<title>" + encodeURI(utils.rmvEntities(article.metadata.title)) + "</title>" + CONFIG.BREAK;
+        xml += CONFIG.INDENT + CONFIG.INDENT + "<title>" + utils.rmvEntities(article.metadata.title) + "</title>" + CONFIG.BREAK;
         xml += CONFIG.INDENT + CONFIG.INDENT + "<link>" + encodeURI(CONFIG.SITE_ROOT + "/article/" + article.path + ".html") + "</link>" + CONFIG.BREAK;
-        xml += CONFIG.INDENT + CONFIG.INDENT + "<description>" + encodeURI(utils.rmvEntities(article.metadata.description)) + "</description>" + CONFIG.BREAK;
+        xml += CONFIG.INDENT + CONFIG.INDENT + "<description>" + utils.rmvEntities(article.metadata.description) + "</description>" + CONFIG.BREAK;
         xml += CONFIG.INDENT + CONFIG.INDENT + "<media:thumbnail url=\"" + encodeURI(CONFIG.SITE_ROOT + article.metadata.banner) + "\"/>" + CONFIG.BREAK;
         xml += CONFIG.INDENT + "</item>";
         return xml;
@@ -39,11 +39,11 @@ module.exports = class RssFeed{
         xml += "<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\"  xmlns:media=\"http://search.yahoo.com/mrss/\">" + CONFIG.BREAK;
         xml += CONFIG.INDENT + "<channel>" + CONFIG.BREAK;
         xml += CONFIG.INDENT + "<atom:link href=\"" + CONFIG.SITE_ROOT + "/feed.xml\" rel=\"self\" type=\"application/rss+xml\"/>" + CONFIG.BREAK;
-        xml += CONFIG.INDENT + "<title>" + encodeURI(this.title) + "</title>" + CONFIG.BREAK;
+        xml += CONFIG.INDENT + "<title>" + this.title + "</title>" + CONFIG.BREAK;
         xml += CONFIG.INDENT + "<link>" + encodeURI(this.link) + "</link>" + CONFIG.BREAK;
-        xml += CONFIG.INDENT + "<description>" + encodeURI(this.description) + "</description>" + CONFIG.BREAK;
+        xml += CONFIG.INDENT + "<description>" + this.description + "</description>" + CONFIG.BREAK;
         xml += CONFIG.INDENT + "<language>de-de</language>" + CONFIG.BREAK;
-        xml += CONFIG.INDENT + "<copyright>" + encodeURI(CONFIG.SITE_NAME) + "</copyright>" + CONFIG.BREAK;
+        xml += CONFIG.INDENT + "<copyright>" + CONFIG.SITE_NAME + "</copyright>" + CONFIG.BREAK;
         for(var i = 0;i < this.articles.length;i++){
             xml += this.renderItem(this.articles[i]) + "" + CONFIG.BREAK;
         }
