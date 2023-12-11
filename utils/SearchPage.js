90c0f6c7245c28b333aa82cc98186224a612cbb0
Saasmull <79657472+Saasmull@users.noreply.github.com>
2023-12-11 15:55:40 +0100

    Better search page

--- utils/SearchPage.js
+++ utils/SearchPage.js
@@ -6,7 +6,9 @@ module.exports = class SearchPage{
     constructor(){}
     render(){
         var page = CONFIG.BASIC_TEMPLATE
-            .replace("<!--METADATA-->","<title>Suchen</title><link rel=\"icon\" type=\"image/png\" href=\"" + CONFIG.LOGO + "\">")
+            .replace("<!--METADATA-->",
+                "<title>Suchen | Journalismus-AG</title><meta name=\"description\" content=\"Durchsuche alle Artikel der Journalismus-AG mit unserer Suchfunktion zu den Themen Politik, Umwelt & etc.\">" + 
+                "<link rel=\"icon\" type=\"image/png\" href=\"" + CONFIG.LOGO + "\">")
             .replace("<!--CONTENT-->",fs.readFileSync("templates/search.html","utf8"));
         return page;
     }
